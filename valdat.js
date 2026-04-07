f77844fa6fe1df6247ee7ccd2108894f662233a2
bayu jaya <ariesofyan55@gmail.com>
2026-04-07 09:02:21 +0700

    update valdat again

--- valdat.js
+++ valdat.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const connection = require('./connection');
 const csv = require('csv-parser');
 const { exec } = require('child_process');
 const { user_aribi, pass_aribi } = require('./login');
